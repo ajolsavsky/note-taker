bbfa61b774ad735336fbe8e892b3b3e97139d372
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-06-17 11:03:06 -0500

    Post request is received

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,32 @@ const notesData = require('../db/db.json');
 // GET Route for retrieving all notes
 notes.get('/', (req, res) => res.json(notesData));
 
+//POST Route
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a new note`)
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting new note')
+    }
+}
+)
+
 console.log(`Here's notes.js ✏️`)
 
 module.exports = notes;
